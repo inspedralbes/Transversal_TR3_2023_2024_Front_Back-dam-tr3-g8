9055692fbd4f0b377210928f53c04b0acab22fa2
a22wilmccash <a22wilmccash@inspedralbes.cat>
2024-03-18 09:04:44 +0100

    WIP settings tab

--- interficie/frontend-videojoc/pages/configuracions-de-joc.vue
+++ interficie/frontend-videojoc/pages/configuracions-de-joc.vue
@@ -5,20 +5,27 @@
 <template>
   <div class="background">
     <div class="main-settings-tab">
-      YEEHAW SETTINGS
+      YEEHAW SETTINGS TAB 1
       <div class="player-settings-tab">
         <div>
           <p><b>PLAYER SETTINGS</b></p>
         </div>
         <div>
+          <p>Player Name</p>
           <p>Max Hit Points(slider)</p>
+          <input class="player-hit-points" type="range">
           <p>Max Shield Points(slider)</p>
+          <input class="player-shield-points" type="range">
           <p>Movement Speed(counter)</p>
           <p>Attack Speed(counter)</p>
           <p>Attack Range(slider)</p>
+          <input class="player-hit-points" type="range">
           <p>Attack Damage(counter)</p>
         </div>
       </div>
+    </div>
+    <div class="main-settings-tab">
+      YEEHAW SETTINGS TAB 2
       <div class="npc-settings-tab">
         <div>
           <p><b>NPC SETTINGS</b></p>
@@ -29,6 +36,7 @@
         <div class="npc-indiv-settings">
           <p>Npc name</p>
           <p>Max Hit Points(slider)</p>
+          <input class="player-hit-points" type="range">
           <p>Movement Speed(counter)</p>
           <p>Attack Damage(counter)</p>
         </div>
