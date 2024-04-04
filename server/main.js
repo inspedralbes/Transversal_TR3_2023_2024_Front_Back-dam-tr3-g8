1394d6ce3db60d159b370e7105da510440047c13
CeliaPedralbes <a22celgariba@inspedralbes.cat>
2024-04-04 10:51:04 +0200

    preparacio docker multijugador

--- server/main.js
+++ server/main.js
@@ -5,13 +5,7 @@ const app = express();
 const server = http.createServer(app);
 const soket = require("socket.io");
 const io = soket(server)
-//const path = require('path');
-const crypto = require('crypto');
-const fs = require('fs');
-//const imatges = require('join-images')
-const jwt = require("jsonwebtoken");
 app.use(express.json({ strict: false }))
-//app.use(bodyParser.json());
 app.use(cors(
   {
     "origin": "*",
@@ -20,153 +14,22 @@ app.use(cors(
     "optionsSuccessStatus": 204
   }
 ))
-const spritesheets = "../assetsNode";
-const operacionsAssets = require("./operacionsMongo/operacionsAssets");
-const operacionsUser = require("./operacionsMongo/operacionsUsuaris");
-const operacionsProta = require("./operacionsMongo/operacionsProtagonista");
-const operacionsEnemic = require("./operacionsMongo/opercionsEnemics");
-const operacionsOdoo = require("./operacionsOdoo/operacionsOdoo");
-const operacionsBroadcast = require("./operacionsMongo/operacionsBroadcast");
+
 const operacionsPartida = require("./operacionsMongo/operacionsPartida")
 
-const PORT = 3817;
+const PORT = 3820;
 
-/*app.listen(PORT, async () => {
-  await operacionsAssets.connexioAssets();
-  await operacionsEnemic.connexioEnemics();
-  await operacionsProta.connexioJugador();
-  await operacionsUser.connexioUsuari();
-  await operacionsBroadcast.connexioBroadcast();
-  console.log(`Server is running on http://localhost:${PORT}`);
-});*/
+
 
 server.listen(PORT, async () => {
   //iniciem les connexions a mongo per no alentir les operacions mes endavant
-  await operacionsAssets.connexioAssets();
-  await operacionsEnemic.connexioEnemics();
-  await operacionsProta.connexioJugador();
-  await operacionsUser.connexioUsuari();
-  await operacionsBroadcast.connexioBroadcast();
   await operacionsPartida.connexioPartida();
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-//---------------------Crides interficie------------------//
-
-app.get("/controlTenda", async (req, res) => {
-  let productes = await operacionsAssets.obtenirAssets();
-  res.json(productes)
-})//envia tots els assets de la bbdd a interficie
-
-app.post("/actualitzarAsset", async (req, res) => {
-  asset = req.body;
-  await operacionsAssets.editarAsset(asset)
-})//reb un asset desde interficie i actualitza el registre a mongo
-
-app.post("/actualitzarProtagonista", async (req, res) => {
-  novesStats = req.body
-  await operacionsProta.actualitzarStatsProta(novesStats)
-})//reb un objecte protagonista i actualitza el de la bbdd
-
-app.post("/actualitzarEnemic", async (req, res) => {
-  novesStats = req.body
-  await operacionsEnemic.actualitzarStatsEnemic(novesStats)
-})//reb un objecte enemic i actualitza el de la bbdd per id
-
-app.post("/broadcast", async (req, res) => {
-  console.log(req.body)
-  missatge = {
-    enunciat: req.body.titol,
-    text: req.body.cos,
-    fecha: new Date()
-  }
-  await operacionsBroadcast.crearBroadcast(missatge)
-
-})//reb un missatge i l'enmagatzema a la base de dades per poder recollir desde android
-
-app.post("/inserirAsset", async (req, res) => {
-  nouAsset = req.body
-  await operacionsAssets.crearAsset(nouAsset)
-})//reb un objecte asset i l'insereix directament a mongo
-
-app.post("/actulitzarSprite", async (req, res) => {
-  let nouSpriteSheet = req.body.imatge
-  let nouNom = spritesheets + "/" + req.body.nom + ".png"
-  fs.writeFile(nouNom, nouSpriteSheet, { encoding: 'base64' }, function (err) {
-    console.log('File created');
-  });
-
-})//substitueix la imatge per la nova
-
-app.get("/mirarSprites", async (req, res) => {
-  let arraySprites = [{}]
-  fs.readdir(spritesheets, function (err, files) {
-    files.forEach(async function (file, index) {
-      arraySprites[index] = {
-        nom: file,
-        imatge: base64_encode(file)
-      }
-    })
-  })
-  arraySprites=JSON.parse(arraySprites)
-  res.json(arraySprites)
-})
 
 
 //---------------------Crides android------------------//
-
-app.post("/loginUser", async (req, res) => {
-  username = req.body.user;
-  passwd = req.body.contrasenya
-  passwd = desencriptar(passwd)
-  let validacio = operacionsUser.logejarUser(username, passwd);
-  if (validacio != null) {
-    let token;
-    //Creating jwt token
-    token = jwt.sign(
-      {
-        userId: validacio.id,
-      },
-      "secretkeyappearshere",
-      { expiresIn: "4h" }
-    )
-    resposta = {
-      permis: true,
-      token: token
-    }
-  }
-  else {
-    resposta = {
-      permis: false,
-      token: null
-    }
-  }
-  res.json(resposta)
-})//permet autoritzar el login del usuari
-
-app.post("/registerUser", async (req, res) => {
-  usuari = req.body.user
-  contrasenya = encriptar(req.body.passwd)
-  email = req.body.correu
-
-})//crea un registre del usuari a la bbdd 
-
-app.get("/veureTenda", async (req, res) => {
-  productes = await operacionsAssets.obtenirTenda()
-  res.json(productes)
-})//agafa els productes en venta i els envia al solicitant
-
-app.get("/veureBroadcasts", async (req, res) => {
-  let missatges = [{}]
-  missatges = await operacionsBroadcast.enviarBroadcast()
-  missatges = missatges.sort((a, b) => {
-    if (a.fecha > b.fecha) {
-      return -1
-    }
-  });
-  res.json(missatges)
-})//reenvia tots els broadcasts de la base de dades a android
-
 
 io.on('connection', (socket, identificacio) => {
   //Utilitzem "identificacio" com el token que obtenen els usuaris a fer login per identificar qui es qui per evitar que el 2n player faci els moviments del primer jugador
@@ -208,43 +71,3 @@ io.on('connection', (socket, identificacio) => {
   })//tanca la sala
 
 })//socket per permetre el multijugador a android, esta preparat per dos jugadors nomes
-
-//---------------------Crides multiplataforma------------------//
-
-app.get("/statsProta", async (req, res) => {
-  stats = await operacionsProta.obtenirStatsProta()
-  res.json(stats)
-})//retorna tots els protagonista
-
-app.get("/statsEnemics", async (req, res) => {
-  stats = await operacionsEnemic.obtenirStatsEnemics()
-  res.json(stats)
-})//retorna tots els enemics
-
-//---------------------Procesos odoo------------------//
-
-
-
-//---------------------Funcions auxiliars------------------//
-
-function encriptar(data) {
-  const cipher = crypto.createCipher('aes-256-cbc', 'a password');
-  let encrypted = cipher.update(data, 'utf8', 'hex');
-  encrypted += cipher.final('hex');
-  return encrypted
-} //encripta la contrasenya per poder guardarla a la bbdd
-
-function desencriptar(data) {
-  const decipher = crypto.createDecipher('aes-256-cbc', 'a password');
-  let decrypted = decipher.update(data, 'hex', 'utf8');
-  decrypted += decipher.final('utf8');
-  return decrypted
-} //desencripta la contrasenya per poder validar el login
-
-function base64_encode(file) {
-  // read binary data
-  var bitmap = fs.readFileSync(file);
-  // convert binary data to base64 encoded string
-  return new Buffer(bitmap).toString('base64');
-}//funcio auxilar per codificar fotos
-
